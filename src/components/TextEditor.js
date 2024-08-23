b77e33f983bb9b93353cff7120c5fd0b71ac49a6
Martin Spicuzza <spicuzza157@gmail.com>
2024-08-23 16:15:21 -0500

    adding state to texteditor

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 const TextEditor = (props) => {
+
+    // State to handle changes in the text editor content
+    const [content, setContent] = useState(props.content ? props.content : "")
+
     const title = props.title ? props.title : "Text Editor";
     const className = props.className ? props.className : "";
     const titleClass = props.titleClass ? props.titleClass : "";
+
+    // Quill modules configuration
+    const modules = {
+        toolbar: [
+          [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+          [{ size: [] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' },
+          { 'indent': '-1' }, { 'indent': '+1' }],
+          ['link', 'image', 'video'],
+          ['clean']
+        ],
+        clipboard: {
+          // toggle to add extra line breaks when pasting HTML:
+          matchVisual: false,
+        }
+      };
 
     return (
         <div>
             <div className={className}>
                <h2 className={titleClass}>{title}</h2>
-               <ReactQuill theme='snow'/>
             </div>
+
+            <ReactQuill 
+            className="h-[10rem]" 
+            theme='snow'
+            formats={['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'indent', 'link', 'image', 'video']}
+            placeholder="Write something amazing..."
+            modules={modules}
+            onChange={setContent} // Handle text editor content changes
+            value={content} // Set the value of the text editor in content
+            />
+
         </div>
     );
 };
